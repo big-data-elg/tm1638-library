556c81bfab3c6a6df6e01ab9da173c0178a16ad2
Yongbum Lee <2yongbum@gmail.com>
2019-07-19 20:03:58 +0900

    inno: fix ESP8266 build error

--- TM16XX.h
+++ TM16XX.h
@@ -25,6 +25,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	#include "WProgram.h"
 #endif
 
+#ifdef ESP8266
+#define min _min
+#define max _max
+#endif
+#define BUFFER_LENGTH 64
+
 #include "TM16XXFonts.h"
 
 class TM16XX
